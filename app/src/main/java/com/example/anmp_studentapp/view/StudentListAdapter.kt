ef91e8138351c391c7417c55360f2d1eac56e5d5
RevaRayhan <revarayhansyah@gmail.com>
2024-06-03 08:12:24 +0700

    W11 - Data Binding

--- app/src/main/java/com/example/anmp_studentapp/view/StudentListAdapter.kt
+++ app/src/main/java/com/example/anmp_studentapp/view/StudentListAdapter.kt
@@ -12,7 +12,7 @@ import com.squareup.picasso.Callback
 import com.squareup.picasso.Picasso
 import java.lang.Exception
 
-class StudentListAdapter(val studentList:ArrayList<Student>):RecyclerView.Adapter<StudentListAdapter.StudentViewHolder>() {
+class StudentListAdapter(val studentList:ArrayList<Student>):RecyclerView.Adapter<StudentListAdapter.StudentViewHolder>(), ButtonClickListener {
     class StudentViewHolder(var binding:StudentListItemBinding):RecyclerView.ViewHolder(binding.root)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StudentViewHolder {
@@ -25,32 +25,42 @@ class StudentListAdapter(val studentList:ArrayList<Student>):RecyclerView.Adapte
     }
 
     override fun onBindViewHolder(holder: StudentViewHolder, position: Int) {
-        with(holder.binding) {
-            val picasso = Picasso.Builder(holder.itemView.context)
-            picasso.listener { picasso, uri, exception -> exception.printStackTrace()}
-            picasso.build().load(studentList[position].photoUrl).into(imgStudent, object:Callback {
-                override fun onSuccess() { //Jika proses loading gambar sudah selesai
-                    progressImage.visibility = View.INVISIBLE
-                    imgStudent.visibility = View.VISIBLE
-                }
-                override fun onError(e: Exception?) {
-                    Log.e("picasso_error", e.toString())
-                }
+        holder.binding.student = studentList[position]
+        holder.binding.listener = this
 
-            })
 
-            txtID.text = studentList[position].id
-            txtName.text = studentList[position].name
-            btnDetail.setOnClickListener {
-                val action = StudentListFragmentDirections.actionDetailFragment(studentList[position].id.toString())
-                Navigation.findNavController(it).navigate(action)
-            }
-
-        }
+//        with(holder.binding) {
+//            val picasso = Picasso.Builder(holder.itemView.context)
+//            picasso.listener { picasso, uri, exception -> exception.printStackTrace()}
+//            picasso.build().load(studentList[position].photoUrl).into(imgStudent, object:Callback {
+//                override fun onSuccess() { //Jika proses loading gambar sudah selesai
+//                    progressImage.visibility = View.INVISIBLE
+//                    imgStudent.visibility = View.VISIBLE
+//                }
+//                override fun onError(e: Exception?) {
+//                    Log.e("picasso_error", e.toString())
+//                }
+//
+//            })
+//
+//            txtID.text = studentList[position].id
+//            txtName.text = studentList[position].name
+//            btnDetail.setOnClickListener {
+//                val action = StudentListFragmentDirections.actionDetailFragment(studentList[position].id.toString())
+//                Navigation.findNavController(it).navigate(action)
+//            }
+//
+//        }
     }
     fun updateStudentList(newStudentList:ArrayList<Student>) {
         studentList.clear()
         studentList.addAll(newStudentList)
         notifyDataSetChanged()
+    }
+
+    override fun onButtonClick(v: View) {
+        val id = v.tag.toString()
+        val action = StudentListFragmentDirections.actionDetailFragment(id)
+        Navigation.findNavController(v).navigate(action)
     }
 }
---
+++ app/src/main/java/com/example/anmp_studentapp/view/interface.kt
@@ -0,0 +1,7 @@
+package com.example.anmp_studentapp.view
+
+import android.view.View
+
+interface ButtonClickListener {
+    fun onButtonClick(v:View)
+}
